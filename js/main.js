6233a86d343a8ecd13101102d4d97687d5016e0d
alex <alexlionstudio@gmail.com>
2017-11-29 23:52:54 +0500

    nav_3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
 	//Slde2id - плавная прокрутка
-	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	$("div a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
 	    highlightSelector:"nav a"
 	})
-
-
 });
