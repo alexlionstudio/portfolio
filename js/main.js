036f82fe7116af75965555998ab1a41821a962fe
alex <alexlionstudio@gmail.com>
2017-12-01 21:18:11 +0500

    header_2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	//Slde2id - плавная прокрутка
-	$("div a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	$("a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
 	    highlightSelector:"nav a"
 	})
 
